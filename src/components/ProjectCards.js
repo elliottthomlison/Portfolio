8d2f8acdd2ac0e23ab3d226f8a4fa8095c51a7e1
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-03-21 11:22:16 -0700

    Attempt to adjust component for reponsivity reasons

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -19,7 +19,7 @@ const ProjectCards = () => {
         <h1 className="relative text-6xl text-blue-900 flex justify-center cursive pt-20 hover:text-gray-900"></h1>
         <h2 className="relative text-4xl text-gray-900 flex justify-center hover:text-gray-900 py-5">
         </h2>
-      <div className="container-fluid d-flex justify-content-center ">
+      {/* <div className="container-fluid d-flex justify-content-center "> */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12 bg-gray-900 px-4 ">
           <div>
           <Project imgsrc={TiniestLittleApp} title="Tiniest Little App" description="TiniestLittleApp is a multipage app that creates miniaturized urls." link="https://tinyurl.com/9xjjh93d" github="Check out the repository👈"/>
@@ -30,11 +30,11 @@ const ProjectCards = () => {
         <div>
           <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tapioca Loca is a minimalist single-page app." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
         </div>
-        {/* <div className="col-md-3 pt-5">
+        <div>
           <Project imgsrc={Interview} title="Interview Scheduler" description="Interview Scheduler is a React app that users can book and cancel interviews." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
-        </div> */}
+        </div>
        </div>
-      </div>
+      {/* </div> */}
       <div className="container-fluid d-flex justify-content-center">
        <div className="row bg-gray-900 px-4">
           <div className="col-md-3 py-5">
