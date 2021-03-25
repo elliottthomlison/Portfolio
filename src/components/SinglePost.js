4111fc6101955f258e3ca740f55e10b0632ce157
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-03-24 17:49:20 -0700

    Adjust the design of the blog post cards

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -38,21 +38,21 @@ export default function SinglePost() {
   if (!singlePost) return <div>Loading...</div>;
 
   return (
-    <main>
-      <article>
-        <header>
-          <div>
-            <div>
-              <h1>
+    <main className="bg-gray-200 min-h-screen p-12">
+      <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <header className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h1 className="cursive text-3xl lg:text-6xl mb-4">
                 {singlePost.title}
               </h1>
-              <div>
+              <div className="flex justify-center text-gray-800">
                 <img
                   src={urlFor(singlePost.authorImage).url()}
                   alt={singlePost.name}
                   className="w-10 h-10 rounded-full"
                 />
-                <p>
+                <p className="cursive flex items-center pl-2 text-2xl">
                   {singlePost.name}
                 </p>
               </div>
@@ -65,7 +65,7 @@ export default function SinglePost() {
             style={{ height: "400px" }}
           />
         </header>
-        <div>
+        <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
           <BlockContent
             blocks={singlePost.body}
             projectId="r99w5jgb"
