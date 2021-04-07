ee239a2e3a602703393b2f26fa853f1758f4e7bf
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-06 20:11:27 -0700

    Adjust the projects component's styling

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -15,46 +15,42 @@ import Codewars from "../images/Codewars.png"
 
 const ProjectCards = () => {
     return (
-      <div class="flex flex-wrap -mx-5 overflow-hidden sm:-mx-5 md:-mx-5 lg:-mx-5 xl:-mx-5">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-2 pt-20">
 
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  <Project imgsrc={TiniestLittleApp} title="Tiniest Little App" description="TiniestLittleApp is a multipage app that creates miniaturized urls." link="https://tinyurl.com/9xjjh93d" github="Check out the repository👈"/>
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3"> <Project imgsrc={Tweeter} title="Tweeter" description="Tweeter is a simple, single-page Twitter clone." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3"> <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tapioca Loca is a minimalist single-page app." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3"> <Project imgsrc={Interview} title="Interview Scheduler" description="Interview Scheduler is a React app that users can book and cancel interviews." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-  <div class="my-5 px-5 w-1/3 overflow-hidden sm:my-5 sm:px-5 sm:w-1/3 md:my-5 md:px-5 md:w-1/3 lg:my-5 lg:px-5 lg:w-1/3 xl:my-5 xl:px-5 xl:w-1/3">
-  </div>
-
-</div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={TiniestLittleApp} title="Tiniest Little App" description="TiniestLittleApp is a multipage app that creates miniaturized urls." link="https://tinyurl.com/9xjjh93d" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={Tweeter} title="Tweeter" description="Tweeter is a simple, single-page Twitter clone." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tapioca Loca is a minimalist single-page app." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={Interview} title="Interview Scheduler" description="Interview Scheduler is a React app that users can book and cancel interviews." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={Jungle} title="Jungle" description="Jungle is a mini e-commerce application built with Rails and Ruby on Rails." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={MiCasa} title="MiCasa" description="MiCasa is a mobile app that helps individuals find a home to rent." link="https://tinyurl.com/tzrk9re6" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">  
+            <Project imgsrc={MiTienda} title="MiTienda" description="MiTienda is an e-commerce app that allows users to shop." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded"> 
+            <Project imgsrc={MiCine} title="MiCine" description="MiCine is a movie-searching app where users can read and see ratings." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
+          </div>
+          <div className="bg-white p-3 rounded">
+            <Project imgsrc={Codewars} title="Codewars" description="Codewars contains solutions of problems that I solved." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="bg-white p-3 rounded">1</div>
+          <div className="bg-white p-3 rounded">1</div>
+          <div className="bg-white p-3 rounded">1</div>
+          
+        </div>
+      </div>
     )
   }
 
