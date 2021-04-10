ceaa2315116169dc416af264d8af213e35555a45
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-09 18:47:08 -0700

    Move the hamburger farther left via padding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,37 +7,36 @@ import Ripple from './Ripple.js'
 // these are the links that will be available in the navbar
 export default function NavBar() {
   return (
-    
     <nav class="bg-gray-900 py-10">
-   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-  <header class="flex items-center bg-gray-900 ">
-    <div class="flex-1 flex justify-between items-center">
-    <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-2 px-2 text-blue-100 hover:text-gray-900 text-6xl font-bold cursive tracking-widest">
-              <Ripple >Elliott's Corner</Ripple>
-            </NavLink> 
-    </div>
-
-    <label for="menu-toggle" class="pointer-cursor md:hidden block">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"/></svg>
-    </label>
-    <input class="hidden" type="checkbox" id="menu-toggle" />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <header class="flex items-center bg-gray-900 ">
+        <div class="flex-1 flex justify-between items-center">
+        <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-2 px-2 text-blue-100 hover:text-gray-900 text-6xl font-bold cursive tracking-widest">
+          <Ripple >
+            Elliott's Corner
+          </Ripple>
+        </NavLink> 
+        </div>
+        <div className="pr-10">
+          <label for="menu-toggle" class="pointer-cursor md:hidden block">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"/></svg>
+          </label>
+          <input class="hidden" type="checkbox" id="menu-toggle" />
+        </div>
 
     <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
       <nav>
       <NavLink to="/project" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-6 text-blue-100 hover:text-blue-900 text-4xl font-bold cursive tracking-widest">
-              <Ripple >Projects</Ripple>
+            <Ripple >Projects</Ripple>
             </NavLink>        
-            
             <NavLink to="/post" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-6 text-blue-100 hover:text-blue-900 text-4xl font-bold cursive tracking-widest"> 
               <Ripple >Blog Posts</Ripple>
             </NavLink>
-            
             <a id="resumeButton" className="no-underline button text-white inflex-flex items-center py-6 px-6 text-blue-100 hover:text-blue-900 text-4xl font-bold cursive tracking-widest">
               <Ripple >
             <td onClick={()=> window.open("https://drive.google.com/file/d/1ULhMdc-i-7YZMNFTAbuVqKMmD0xtnKcu/view?usp=sharing")}>Resume</td>
               </Ripple>
             </a>
-            
             <a className="inflex-flex items-center py-6 px-6 text-blue-100 hover:text-blue-900 text-4xl font-bold cursive tracking-widest" href="mailto: elliott.thomlison@gmail.com">
               <Ripple>Contact Me</Ripple>
             </a>     
