be20ff8d0d0e6d327f575fc6b5c6c6e4a37981ab
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-16 11:30:38 -0700

    Add a tags to each project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './projectCards.css' 
 
 const Project = props =>{
+
   return (
-    
     <div className="card text-center">
       <div className="overflow">
         <img src={props.imgsrc} className="card-img-top"/>
@@ -12,10 +12,12 @@ const Project = props =>{
        <h4 className="card-title">{props.title}</h4>
        <p className="card-text text-secondary">{props.description}
        </p>
-       <a href="https://tinyurl.com/dadzmpec" className="btn btn-outline-success">{props.github}</a>
+       <a href={props.stuff} target="_blank"   className="btn btn-outline-success">Check out the repository👈</a>
      </div>
     </div>
+
   )
 }
+
 
 export default Project
