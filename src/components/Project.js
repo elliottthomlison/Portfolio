ed16280dd5302d76a649ea345d71d85a13b4eb4c
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-17 12:40:38 -0700

    Make sure that the image clicks open a new tab

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,16 +6,16 @@ const Project = props =>{
   return (
     <div className="card text-center">
       <div className="overflow">
-        <a href={props.link}>
-          <img src={props.imgsrc} target="_blank" className="card-img-top" />
+        <a href={props.link} target="_blank">
+          <img src={props.imgsrc} className="card-img-top" />
         </a>
       </div>
-     <div className="card-body text-dark">
-       <h4 className="card-title">{props.title}</h4>
-       <p className="card-text text-secondary">{props.description}
-       </p>
-       <a href={props.link} target="_blank" className="btn btn-outline-dark">Check out the repository👈</a>
-     </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text text-secondary">{props.description}
+        </p>
+        <a href={props.link} target="_blank" className="btn btn-outline-dark">Check out the repository👈</a>
+      </div>
     </div>
 
   )
