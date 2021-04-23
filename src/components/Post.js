9ad224a67f48bbf7e785dbee1e40cc1a8a4fb9a8
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-22 19:57:04 -0700

    Add further functionality to display and produce posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import sanityClient from '../client.js';
 
+// flow of data => state, usestate, setstate
 export default function Post() {
-  return <h1>Post Page</h1>
+  // set is called postData, setting our state is is called setpost
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+    // * = all | grab all titles with the title of post | 
+      .fetch(`*[_type == 'post]{
+        title,
+        slug,
+        mainImage {
+          asset-> {
+            _id, 
+            url
+          },
+          alt
+        }
+      }`)
+      //fetch the data then...
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+        <div className="grid md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-1">
+          {/* we will need to map  over all the info below */}
+          {postData && postData.map((post, index) => (
+          <article>
+            {/* whichever current post we're clicking on will be selected via the line below */}
+            <Link to={'/post/' + postData.slug.current}>
+            <span className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400' key={index}>
+              <img 
+              //if I wanted to have a consistent wallpaper i could bring it in here like i did for homepage, but if i want to have a specific background for each post/blog post then i will use this part below
+              src={post.mainImage.asset.url}
+              alt={post.mainImage.alt}
+              className='w-full h-full rounded-r object-cover absolute'
+              />
+              <span className='block relative h-full flex justify-end items-end pr-4 pb-4'>
+                <h3 className='text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opactiy-75 rounded'>
+                  {post.title}
+                </h3>
+              </span>
+            </span>
+            </Link>
+          </article>
+          ))}
+        </div>
+      </section>
+    </main>
+  )
 }
