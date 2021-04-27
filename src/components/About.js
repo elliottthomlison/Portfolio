cad00e574112d2c0226e7390cf2f0de54d7e64a8
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-26 20:19:04 -0700

    Add further styling changes (colors, borders, hover effects)

--- src/components/About.js
+++ src/components/About.js
@@ -29,19 +29,21 @@ export default function About() {
   return (
     <main className="relative">
       <img src={bg1} alt="background" className="absolute w-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
+      <div className="lg:pt-20 container mx-auto relative">
         <section className="bg-white-600 rounded-lg shadow-1xl lg:flex p-10">
-          <img
-            src={urlFor(author.authorImage).url()}
-            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
-            alt="Kapehe"
-          />
+          
           <div className="text-lg flex flex-col justify-center">
             <h1 className="cursive text-6xl text-gray-300 mb-4 hover:text-gray-900">
-              Hey there. Once again I'm{" "} 
-              <span className="text-gray-900">{author.name}</span> and this is me.
+              Hey there. Once again, I'm <span className="cursive text-6xl text-gray-100">Elliott</span>. Please recognize the two T's.
             </h1>
-            <p className="text-green-100">{author.bio}</p>
+            <p className="cursive text-3xl text-gray-100 hover:text-gray-900">I am a Canadian man trying to evolve from Pinky to The Brain. A transformation that I hope will take place as I continually make progress and expand my knowledge of CSS, HTML, JavaScript, React, along with the other languages and frameworks that I know.
+            </p>
+            
+            <br></br>
+            
+            <p className="cursive text-3xl text-gray-100 hover:text-gray-900">
+            I've lived in Quebec (Montréal and Trois-Rivières), South Korea (Daejeon), and now I've returned home to Vancouver. I enjoy throwing frisbees, attempting muscle-ups, reading, attempting my hand at art, and the mental challenge of coding. I will become increasingly competent as a developer and look forward to finding a team to expedite that process.
+            </p>
           </div>
         </section>
       </div>
