850282d0d9edf45760acdbef4b61728de0b3f4d5
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-18 15:19:30 -0700

    Create visual changes to the app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,30 +10,33 @@ export default function NavBar() {
       <div className="container m-0.5 flex justify-between">
         <nav className="flex">
           {/* py = padding on the y axis */}
-          <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
+          <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-1 px-6 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
             Elliott's Corner
           </NavLink>
 
-          <NavLink to="/post" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
+          <NavLink to="/post" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-6 mr-4 text-blue-100 hover:text-gray-900 text-4xl font-bold cursive tracking-widest">
             
             Blog Posts
           </NavLink>
 
-          <NavLink to="/project" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
+          <NavLink to="/project" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-6 mr-4 text-blue-100 hover:text-gray-900 text-4xl font-bold cursive tracking-widest">
             Projects
           </NavLink>
 
-          <NavLink to="/about" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
+          <NavLink to="/about" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-6 text-blue-100 hover:text-gray-900 text-4xl font-bold cursive tracking-widest">
             About Me!
           </NavLink>
         </nav>
-        {/* use a npm download (react-social-icons) to display these images */}
-        <div className="py-3 my-6 mx9">
-          <SocialIcon url='https://www.linkedin.com/in/elliott-thomlison/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
-          <SocialIcon url='https://github.com/elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/> 
-          <SocialIcon url='https://medium.com/@elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
-        </div>
+        {/* use a npm download (react-social-icons) to display these images - absolute top-0 right-0 h-16 w-16*/}
+        <div className="sm:self-center sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0 hidden">
+        
+        <SocialIcon url='https://www.linkedin.com/in/elliott-thomlison/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
+        <SocialIcon url='https://github.com/elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/> 
+        <SocialIcon url='https://medium.com/@elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
+        
       </div>
+      </div>
+      
     </header>
   )
 }
