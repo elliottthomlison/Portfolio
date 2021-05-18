f570ff73c84ff03f43fd1caba40443cd4cb93d37
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-18 00:46:31 -0700

    Further stylistic changes around the navbar and aboutme page

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ export default function Home() {
     <main className="relative">
       <img src={image} alt="background" className="absolute w-full" />
       <div className="lg:pt-20 container mx-auto relative">
-        <section className="bg-white-600 rounded-lg shadow-1xl lg:flex p-10">
-          
-          <div className="text-lg flex flex-col justify-center">
+        <section className=" ">
+          <div class="container mx-auto p-5 bg-blue-600	bg-opacity-50">
+          <div className="text-lg flex flex-col justify-center op">
             <h1 className="cursive text-6xl text-gray-300 mb-4 hover:text-gray-900">
               Hey there. Once again, I'm <span className="cursive text-6xl text-gray-100">Elliott</span>. Please recognize the two T's.
             </h1>
@@ -20,6 +20,7 @@ export default function Home() {
             <p className="cursive text-3xl text-gray-100 hover:text-gray-900">
             I've lived in Quebec (Montréal and Trois-Rivières), South Korea (Daejeon), and now I've returned home to Vancouver. I enjoy throwing frisbees, attempting muscle-ups, reading, attempting my hand at art, and the mental challenge of coding. I will become increasingly competent as a developer and look forward to finding a team to expedite that process.
             </p>
+          </div>
           </div>
         </section>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,28 +7,28 @@ export default function NavBar() {
   return (
     <header className="bg-blue-600">
       {/* mx-auto / flex = puts things in the div / justify-between = spacing between /  */}
-      <div className="container mx-auto flex justify-between">
+      <div className="container m-0.5 flex justify-between">
         <nav className="flex">
           {/* py = padding on the y axis */}
-          <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-4xl font-bold cursive tracking-widest">
+          <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
             Elliott's Corner
           </NavLink>
 
-          <NavLink to="/post" className="inflex-flex items-center py-3 px-3 my-6 text-blue-100 hover:text-gray-900" activeClassName="text-blue-100 bg-blue-700">
+          <NavLink to="/post" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
             
             Blog Posts
           </NavLink>
 
-          <NavLink to="/project" className="inflex-flex items-center py-3 px-3 my-6 text-blue-100 hover:text-gray-900" activeClassName="text-blue-100 bg-blue-700">
+          <NavLink to="/project" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
             Projects
           </NavLink>
 
-          <NavLink to="/about" className="inflex-flex items-center py-3 px-3 my-6 text-blue-100 hover:text-gray-900" activeClassName="text-blue-100 bg-blue-700">
+          <NavLink to="/about" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-gray-900 text-5xl font-bold cursive tracking-widest">
             About Me!
           </NavLink>
         </nav>
         {/* use a npm download (react-social-icons) to display these images */}
-        <div className="inline-flex py-3 px-3 my-6">
+        <div className="py-3 my-6 mx9">
           <SocialIcon url='https://www.linkedin.com/in/elliott-thomlison/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
           <SocialIcon url='https://github.com/elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/> 
           <SocialIcon url='https://medium.com/@elliottthomlison' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
