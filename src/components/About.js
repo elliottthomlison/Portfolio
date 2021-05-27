379e3e7c98acd26d870aefd9719618969e967517
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-26 18:13:47 -0700

    Continue to work on the contact form functionality and style

--- src/components/About.js
+++ src/components/About.js
@@ -1,134 +1,101 @@
-import { useForm } from 'react-hook-form';
-import emailjs from 'emailjs-com';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
-
-export default function About() {
-    const { register, errors, handleSubmit, reset } = useForm();
-
-  const toastifySuccess = () => {
-    toast('Form sent!', {
-      position: 'bottom-right',
-      autoClose: 5000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: false,
-      className: 'submit-feedback success',
-      toastId: 'notifyToast'
-    });
-  };
-
-  const onSubmit = async (data) => {
-    // Send form email
-    try {
-      const templateParams = {
-        name: data.name,
-        email: data.email,
-        subject: data.subject,
-        message: data.message
-      };
-
-      await emailjs.send(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        templateParams,
-        process.env.REACT_APP_USER_ID
-      );
-
-      reset();
-      toastifySuccess();
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div className='ContactForm'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            <div className='contactForm'>
-              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
-                {/* Row 1 of form */}
-                <div className='row formRow'>
-                  <div className='col-6'>
-                    <input
-                      type='text'
-                      name='name'
-                      ref={register({
-                        required: { value: true, message: 'Please enter your name' },
-                        maxLength: {
-                          value: 30,
-                          message: 'Please use 30 characters or less'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Name'
-                    ></input>
-                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
-                  </div>
-                  <div className='col-6'>
-                    <input
-                      type='email'
-                      name='email'
-                      ref={register({
-                        required: true,
-                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                      })}
-                      className='form-control formInput'
-                      placeholder='Email address'
-                    ></input>
-                    {errors.email && (
-                      <span className='errorMessage'>Please enter a valid email address</span>
-                    )}
-                  </div>
-                </div>
-                {/* Row 2 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <input
-                      type='text'
-                      name='subject'
-                      ref={register({
-                        required: { value: true, message: 'Please enter a subject' },
-                        maxLength: {
-                          value: 75,
-                          message: 'Subject cannot exceed 75 characters'
-                        }
-                      })}
-                      className='form-control formInput'
-                      placeholder='Subject'
-                    ></input>
-                    {errors.subject && (
-                      <span className='errorMessage'>{errors.subject.message}</span>
-                    )}
-                  </div>
-                </div>
-                {/* Row 3 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <textarea
-                      rows={3}
-                      name='message'
-                      ref={register({
-                        required: true
-                      })}
-                      className='form-control formInput'
-                      placeholder='Message'
-                    ></textarea>
-                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
-                  </div>
-                </div>
-                <button className='submit-btn' type='submit'>
-                  Submit
-                </button>
-              </form>
-            </div>
-            <ToastContainer />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+  import React, {useState} from 'react';
+  import {
+      ModalWrap,
+      ModalContentWrap,
+      HeaderWrap,
+      FormWrap,
+      InputWrap,
+      InputField,
+      TextArea,
+      Button
+  } from './styles/contactModalStyles';
+  import close from '../assets/close.png';
+  import CircularProgress from '@material-ui/core/CircularProgress';
+  import ErrorMessage from './errorMessage';
+  import FormSuccess from './formSuccess';
+  
+  const ContactModal = (props) => {
+      const [name, setName] = useState('');
+      const [email, setEmail] = useState('');
+      const [multiline, setMultiline] = useState('');
+      const [formSent, setFormSent] = useState(false);
+      const [isLoading, setIsLoading] = useState(false);
+      const [wasError, setWasError] = useState(false);
+  
+      const handleSubmit = e => {
+          e.preventDefault();
+          const req = {
+              from_name: name,
+              from_email: email,
+              message: multiline
+          }
+  
+          setIsLoading(true);
+          sendEmail(req);
+      }
+  
+      const sendEmail = req => {
+          const template = process.env.REACT_APP_EMAILSJS_TEMPLATEID;
+          const userId = process.env.REACT_APP_EMAILJS_USERID;
+          const params = req;
+  
+          window.emailjs.send('default_service', template, params, userId)
+            .then(res => {
+              setIsLoading(false);
+              setFormSent(true);
+              console.log(res);
+            }).catch(e => {
+              setIsLoading(false);
+              setWasError(true);
+              console.log(e)
+            });
+        }
+  
+      return (
+          <ModalWrap>
+              <ModalContentWrap>
+                      <HeaderWrap>
+                          <h4>Contact Me</h4>
+                          <img id='close' src={close} alt='close icon' onClick={props.handleClose} />
+                      </HeaderWrap>
+                      {isLoading ?
+                          <CircularProgress className='loading' />
+                          : wasError ?
+                          <ErrorMessage />
+                          : formSent ? <FormSuccess />
+                          : <>
+                          <FormWrap noValidate autoComplete="off" onSubmit={handleSubmit}>
+                              <InputWrap>
+                                  <InputField
+                                      type='text'
+                                      name='name'
+                                      placeholder='Name'
+                                      value={name || ''}
+                                      onChange={e => setName(e.target.value)}
+                                  />
+                                  <InputField
+                                      type='email'
+                                      name='email'
+                                      placeholder='Email'
+                                      value={email || ''}
+                                      onChange={e => setEmail(e.target.value)}
+                                  />
+                              </InputWrap>
+                              <TextArea
+                                  type='text'
+                                  name='multiline'
+                                  placeholder='Tell me about your project'
+                                  value={multiline || ''}
+                                  onChange={e => setMultiline(e.target.value)}
+                              />
+  
+                              <Button>Send</Button>
+                          </FormWrap>
+                      </>
+                      }
+              </ModalContentWrap>
+          </ModalWrap>
+      )
+  }
+  
+export default About
---
+++ src/components/errorMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorMessage = () => {
+    return <div>Error Sending Email</div>
+}
+
+export default ErrorMessage
