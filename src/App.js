12e3dea59cdfdf566d0686e3649e60a5698fe89b
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-30 09:58:27 -0700

    Create the progressive loading image

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import Post from "./components/Post";
 import Project from "./components/Project";
 // calling the navbar below here and placing inside the function
 import NavBar from "./components/NavBar";
+import ProgressiveImage from "./components/ProgressiveImage";
 
 function App() {
   return (
   <BrowserRouter>
-  <NavBar />
+<ProgressiveImage
+  preview={`https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80`}
+  image={`https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80`}
+/>  <NavBar />
   {/* Switch will switch to allow the components to display for the user */}
     <Switch>
       {/* Need 5 routes because there will be 5 different pages | excluding navbar because it will always be visible*/}
