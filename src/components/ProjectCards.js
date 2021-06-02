37281f8523cf6a0a765d0780ab33f8ef6640f1cb
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-02 00:18:12 -0700

    Remove unnecessary lines of code

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -8,9 +8,8 @@ class ProjectCards extends Component {
     return (
       
       <section>
-        <h1 className="relative text-6xl text-blue-900 flex justify-center cursive pt-20 hover:text-gray-900">Welcome to my Projects Page!</h1>
+        <h1 className="relative text-6xl text-blue-900 flex justify-center cursive pt-20 hover:text-gray-900"></h1>
         <h2 className="relative text-4xl text-gray-900 flex justify-center hover:text-gray-900">
-          Here I share my projects from big to small
         </h2>
       <div className="container-fluid d-flex justify-content-center">
        <div className="row">
