53f4b21776fd361160013ae2cc1d36e67bda36d1
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-03 14:10:56 -0700

    Adjust the typewriting animation found on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [state, setState] = useState({value:''}) 
 
   useEffect(() => {
-    axios.get('https://api.countapi.xyz/update/hellothere/elliott2.netlify.app/?amount=1')
+    axios.get('https://api.countapi.xyz/update/hellothere/ethomlison.netlify.app/?amount=1')
     .then(function (response) {
       setState(response.data);
       console.log(state)
@@ -24,33 +24,50 @@ export default function Home() {
       <div className="lg:pt-20 container mx-auto relative ">
         <h1 className ="text-6xl text-green-900 font-bold cursive lg:leading-snug home-name hover:text-gray-900 text-center"></h1>
         <section class="mt-10">
-          <div >
           <div className="text-lg flex flex-col justify-center op">
-            <h1 className="text-4xl text-gray-900 mb-4 hover:text-gray-900">
-
-            {/* https://www.npmjs.com/package/typewriter-effect */}
-            <p>Hi there, I'm Elliott. <Typewriter
-              options={{
-                strings: ['I am a frontend developer.', 'I am a constant learner.', 'I am transitioning my career into frontend development.', 'I am able to speak French, Korean, and soon Mandarin.', 'I have lived throughout Quebec, Ontario, and South Korea.', 'Yet, I am Canadian and have returned to Vancouver.', 'I am a lover of Cadbury mini eggs.', 
-              ],
-                autoStart: true,
-                loop: true,
-                deleteSpeed: 20,
-                pauseFor: 600,
+            <h1 className="text-4xl text-gray-900 mb-4 hover:text-gray-900" id="test">
+            <h2 id="test">
+              <span 
+              style={{
+                display: 'inline-block',
+              }}
+              
+              ><Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString('I am Elliott.')
+                  .pauseFor(500)
+                  .deleteAll()
+                typewriter.typeString('I am a frontend developer.')
+                  .pauseFor(500)
+                  .deleteAll()
+                typewriter.typeString('I was previously a teacher and corrections officer.')
+                  .pauseFor(500)
+                  .deleteAll()
+                typewriter.typeString('I have learnt to speak French, Korean, and currently I am studying Mandarin.')
+                  .pauseFor(500)  
+                  .deleteAll()
+                typewriter.typeString('I am a lover of Cadbury mini eggs.')              
+                  .pauseFor(500)   
+                  .deleteAll() 
+                typewriter.typeString('I am your new employee.')              
+                  .pauseFor(500)   
+                  .start()         
               }}
             />
-            </p>
+            </span>
+            </h2>
             </h1>
 
             <p className=" text-3xl text-gray-900 hover:text-gray-900">I am a recent <a href id="LHL" onClick={()=> window.open("www.lighthouselabs.ca")}>Lighthouse Labs</a> Web Development Bootcamp alumni. During the bootcamp I learned CSS, HTML, JavaScript, React, jQuery, Ajax, SQL, React, Ruby, Ruby-on-Rails, and other software and skills both in the frontend and backend. I created multiple projects individually and collaboratively, as can be seen in the projects tab. I am eager to find a workplace where I can continue to learn and develop my skills, first as a frontend developer and later as a full stack developer. 
             </p>
           </div>
-          </div>
+          
             </section>
+
             
-      </div>
       <div id="counter">
       {state.value} visitors have seen the magic of this page
+      </div>
       </div>
     </main>
   );
