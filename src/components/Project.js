fc085d1691166ee12968497ee8ddfa318ee1d944
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-03 20:09:56 -0700

    Attach the gifs of the new projects and the changes to the projects portion

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,92 +12,10 @@ const Project = props =>{
        <h4 className="card-title">{props.title}</h4>
        <p className="card-text text-secondary">{props.description}
        </p>
-       <a href="#" className="btn btn-outline-success">{props.github}</a>
+       <a href="https://tinyurl.com/dadzmpec" className="btn btn-outline-success">{props.github}</a>
      </div>
     </div>
   )
 }
 
 export default Project
-
-
-// export default function Project() {
-//   const [projectData, setProjectData] = useState(null);
-
-//   useEffect(() => {
-//     sanityClient
-//       .fetch(
-//         `*[_type == "project"]{
-//           title,
-//           date,
-//           place,
-//           description,
-//           projectType,
-//           link,
-//           tags
-//       }`
-//       )
-//       .then((data) => setProjectData(data))
-//       .catch(console.error);
-//   }, []);
-
-//   return (
-//     <main className="absolute object-cover w-full h-full">
-
-//   {/* <img src={bg2} alt="background" className="absolute bg-repeat"/> */}
-
-//       <section className="container mx-auto">
-//         <h1 className="relative text-6xl text-blue-900 flex justify-center cursive pt-20 hover:text-gray-900">Welcome to my Projects Page!</h1>
-//         <h2 className="relative text-4xl text-gray-900 flex justify-center mb-12 pb-10 hover:text-gray-900">
-//           Here I share my projects from big to small
-//         </h2>
-//         <section className="grid grid-cols-3 gap-5">
-//           {projectData &&
-//             projectData.map((project, index) => (
-//               <article className="bg-gray-400 relative rounded-lg shadow-xl bg-white p-16  bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-//                 <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-//                   <a
-//                     href={project.link}
-//                     alt={project.title}
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                   >
-//                     {project.title}
-//                   </a>
-//                 </h3>
-//                 <div className="text-gray-800 text-xs space-x-4">
-//                   <span>
-//                     <strong className="font-bold">Finished on</strong>:{" "}
-//                     {new Date(project.date).toLocaleDateString()}
-//                   </span>
-//                   <span>
-//                     <strong className="font-bold">Company</strong>:{" "}
-//                     {project.place}
-//                   </span>
-//                   <span>
-//                     <strong className="font-bold">Type</strong>:{" "}
-//                     {project.projectType}
-//                   </span>
-//                   <p className="my-6 text-lg text-gray-700 leading-relaxed">
-//                     {project.description}
-//                   </p>
-//                   <a
-//                     href={project.link}
-//                     rel="noopener noreferrer"
-//                     target="_blank"
-//                     className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
-//                   >
-//                     View The Project{" "}
-//                     <span role="img" aria-label="left pointer">
-//                     👈
-//                     </span>
-//                   </a>
-                   
-//                 </div>
-//               </article>
-//             ))}
-//         </section>
-//       </section>
-//     </main>
-//   );
-// }
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -5,6 +5,7 @@ import './projectCards.css'
 //gifs and images
 import MiTienda from "../images/MiTienda.gif";
 import Tweeter from "../images/Tweeter.gif";
+import TiniestLittleApp from "../images/TiniestLittleApp.gif"
 
 const ProjectCards = () => {
     return (
@@ -13,22 +14,38 @@ const ProjectCards = () => {
         <h2 className="relative text-4xl text-gray-900 flex justify-center hover:text-gray-900">
         </h2>
       <div className="container-fluid d-flex justify-content-center">
-        <div className="row">
+        <div className="row bg-gray-900 px-4">
           <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console" description="Test" github="https://github.com/elliottthomlison/MiTienda👈"/>
+          <Project imgsrc={TiniestLittleApp} title="TiniestLittleApp" description="TiniestLittleApp is a multipage app that creates miniaturized urls. It is the first project made during the Lighthouse Labs Web Development Bootcamp. It is a project that allows one to register, login, and create, delete, and edit the urls that were created. It was created in order to practice using EJS, JS, Express, and NodeJS. It was difficult to comprehend the dynamics of the project initially because of the lack of familiarity with the concepts and implementation steps." link="https://tinyurl.com/9xjjh93d" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
-          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." github="https://tinyurl.com/dadzmpec👈"/>
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/>
+          <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/> 
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
         </div>
        </div>
       </div>
       <div className="container-fluid d-flex justify-content-center">
+       <div className="row bg-gray-900 px-4">
+          <div className="col-md-3">
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+        </div>
+        <div className="col-md-3">
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+        </div>
+        <div className="col-md-3">
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+        </div>
+        <div className="col-md-3">
+          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
+        </div>
+       </div>
+      </div>
+       {/* <div className="container-fluid d-flex justify-content-center">
        <div className="row">
           <div className="col-md-3">
           <Project imgsrc={MiTienda} title="Console"/>
@@ -41,25 +58,9 @@ const ProjectCards = () => {
         </div>
         <div className="col-md-3">
           <Project imgsrc={MiTienda} title="Console"/> 
-        </div>
-       </div>
-      </div>
-      <div className="container-fluid d-flex justify-content-center">
-       <div className="row">
-          <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/>
-        </div>
-        <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/>
-        </div>
-        <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/>
-        </div>
-        <div className="col-md-3">
-          <Project imgsrc={MiTienda} title="Console"/> 
-        </div>
-       </div> 
-      </div>
+        </div>  */}
+       {/* </div> 
+      </div> */}
       </section>
     )
   }
