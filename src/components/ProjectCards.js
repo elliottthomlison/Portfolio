32e3dc6f2a0e9770b3a37e9053471c62d5f09be3
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-03 20:58:41 -0700

    Further customize the project component and the details for each

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,9 +3,13 @@ import Project from './Project.js'
 import './projectCards.css' 
 
 //gifs and images
-import MiTienda from "../images/MiTienda.gif";
-import Tweeter from "../images/Tweeter.gif";
 import TiniestLittleApp from "../images/TiniestLittleApp.gif"
+import Tweeter from "../images/Tweeter.gif";
+import TapiocaLoca from "../images/TapiocaLoca.png"
+import MiCasa from "../images/MiCasa.png"
+import Interview from "../images/Interview.jpeg"
+import Jungle from "../images/Jungle.png"
+import MiTienda from "../images/MiTienda.gif";
 
 const ProjectCards = () => {
     return (
@@ -25,17 +29,17 @@ const ProjectCards = () => {
           <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
-          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
+          <Project imgsrc={Interview} title="Interview" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
         </div>
        </div>
       </div>
       <div className="container-fluid d-flex justify-content-center">
        <div className="row bg-gray-900 px-4">
           <div className="col-md-3">
-          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          <Project imgsrc={Jungle} title="Jungle" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
-          <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          <Project imgsrc={MiCasa} title="MiCasa" description="MiCasa is a mobile app that helps individuals looking to rent to find available units without having to constantly check rental websites. It was is final project that was built during the Lighthouse Labs Web Development Bootcamp. A user sets their preferences and what they're looking for in the desired property, then once they pass that location their receive a cellular notification. MiCasa was built using the PERN stack (Postgre, Express, React, and NodeJS. It aims to help the average person find an appropriate property more easily. This is achieved through the user simply setting the aforementioned preferences to what they look for in a rental, and then passively receiving pings when such properties are found. There were backend specific difficulties during the creation of the app, yet the frontend presented challenges and learning experiences as well." link="https://tinyurl.com/tzrk9re6" github="Check out the repository👈"/>
         </div>
         <div className="col-md-3">
           <Project imgsrc={Tweeter} title="Twitter" description="Tweeter is a simple, single-page Twitter clone. It is the second project that is built during the Lighthouse Labs Web Development Bootcamp. Tweeter takes in the input of the user and prints it out, resembling Twitter in the process. Tweeter was built in order to practice HTML, CSS, JS, jQuery and Ajax front-end skills, along with NodeJS, Express and MongoDB back-end skills. Difficulties arose from inexperience and the use of new tools, the use of a tool to dynamically print the time of each tweet, for example." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
