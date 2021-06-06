8df062b3c47f0665b9749ec051c6a90c85ed7741
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-06 12:27:54 -0700

    Insert the Github repo. links into the projects' card

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -16,41 +16,47 @@ import Codewars from "../images/Codewars.png"
 const ProjectCards = () => {
     return (
       <div className="flex justify-center justify-content">
-        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xl:gap-16 lg:gap-8 md:gap-8">
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
           
-          <div className="pt-32 rounded">
-            <Project imgsrc={TiniestLittleApp} title="Tiniest Little App" description="TiniestLittleApp is a multipage app that creates miniaturized urls." link="https://tinyurl.com/9xjjh93d"/>
+          <div className="pt-32 px-10 ">
+            <Project imgsrc={TiniestLittleApp} title="Tiniest Little App" description="TiniestLittleApp is a multipage app that creates miniaturized urls." link="https://github.com/elliottthomlison/TiniestLittleApp
+"/>
           </div>
-          <div className="pt-32 rounded">
-            <Project imgsrc={Tweeter} title="Tweeter" description="Tweeter is a simple, single-page Twitter clone." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          <div className="pt-32 px-10">
+            <Project imgsrc={Tweeter} title="Tweeter" description="Tweeter is a simple, single-page Twitter clone." link="https://github.com/elliottthomlison/Tweeter" github="Check out the repository👈"/>
           </div>
-          <div className="pt-32 rounded">
-            <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tapioca Loca is a minimalist single-page app." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          <div className="pt-32 px-10">
+            <Project imgsrc={TapiocaLoca} title="Tapioca Loca" description="Tapioca Loca is a minimalist single-page app." link="https://github.com/elliottthomlison/Tapioca-Loca" github="Check out the repository👈"/>
           </div>
-          <div className="pt-32 rounded">
-            <Project imgsrc={Interview} title="Interview Scheduler" description="Interview Scheduler is a React app that users can book and cancel interviews." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
-          </div>
-
-          <div className="pt-20 rounded">
-            <Project imgsrc={Jungle} title="Jungle" description="Jungle is an e-commerce application built with Rails and Ruby on Rails." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
-          </div>
-          <div className="pt-20 rounded">
-            <Project imgsrc={MiCasa} title="MiCasa" description="MiCasa is a mobile app that helps individuals find a home to rent." link="https://tinyurl.com/tzrk9re6" github="Check out the repository👈"/>
-          </div>
-          <div className="pt-20 rounded">  
-            <Project imgsrc={MiTienda} title="MiTienda" description="MiTienda is an e-commerce app that allows users to shop." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
-          </div>
-          <div className="pt-20 rounded"> 
-            <Project imgsrc={MiCine} title="MiCine" description="MiCine is a movie-searching app where users can read and see ratings." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/> 
+          <div className="pt-32 px-10">
+            <Project imgsrc={Interview} title="Interview Scheduler" description="Interview Scheduler is a React app that users can book and cancel interviews." link="https://github.com/elliottthomlison/Interview_Scheduler" github="Check out the repository👈"/> 
           </div>
 
-          <div className="pt-20 rounded">
+          <div className="pt-32 px-10">
+            <Project imgsrc={Jungle} title="Jungle" description="Jungle is an e-commerce application built with Rails and Ruby on Rails." link="https://github.com/elliottthomlison/Jungle" github="Check out the repository👈"/>
+          </div>
+          <div className="pt-32 px-10">
+            <Project imgsrc={MiCasa} title="MiCasa" description="MiCasa is a mobile app that helps individuals find a home to rent." link="https://github.com/elliottthomlison/MiCasa" github="Check out the repository👈"/>
+          </div>
+          <div className="pt-32 px-10">  
+            <Project imgsrc={MiTienda} title="MiTienda" description="MiTienda is an e-commerce app that allows users to shop." link="https://github.com/elliottthomlison/MiTienda" github="Check out the repository👈"/>
+          </div>
+          <div className="pt-32 px-10"> 
+            <Project imgsrc={MiCine} title="MiCine" description="MiCine is a movie-searching app where users can read and see ratings." link="https://github.com/elliottthomlison/MiCine" github="Check out the repository👈"/> 
+          </div>
+
+          <div className="pt-32 px-10">
+            <Project imgsrc={Codewars} title="Codewars" description="Codewars contains solutions of problems that I solved." link="https://github.com/elliottthomlison/Codewars" github="Check out the repository👈"/>
+          </div>
+          <div className="pt-32 px-10">
             <Project imgsrc={Codewars} title="Codewars" description="Codewars contains solutions of problems that I solved." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
           </div>
-          <div className="bg-white pt-20 rounded">1</div>
-          <div className="bg-white pt-20 rounded">1</div>
-          <div className="bg-white pt-20 rounded">1</div>
-          
+          <div className="pt-32 px-10">
+            <Project imgsrc={Codewars} title="Codewars" description="Codewars contains solutions of problems that I solved." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div>
+          <div className="pt-32 px-10">
+            <Project imgsrc={Codewars} title="Codewars" description="Codewars contains solutions of problems that I solved." link="https://tinyurl.com/dadzmpec" github="Check out the repository👈"/>
+          </div> 
         </div>
       </div>
     )
