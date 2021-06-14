d73a5d904d180bc6b8e97a237b10372abe30ddb2
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-13 20:30:31 -0700

    Change the resume link to an updated resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,7 +31,7 @@ export default function NavBar() {
             </NavLink>
             <a id="resumeButton" className="no-underline button text-white inflex-flex items-center py-6 px-6 text-blue-100 text-4xl font-bold cursive tracking-widest">
               <Ripple>
-                <td onClick={()=> window.open("https://drive.google.com/file/d/1Dx9mVp7rAah92HQup1w-1x3P5GShkjNv/view?usp=sharing")}>Resume</td>
+                <td onClick={()=> window.open("https://drive.google.com/file/d/1p1xcotZKKuTobK-ICrxFmr89Z4b8lBr0/view")}>Resume</td>
               </Ripple>
             </a>
             <a className="inflex-flex items-center py-6 px-6 text-blue-100 hover:text-blue-900 text-4xl font-bold cursive tracking-widest" href="mailto: elliott.thomlison@gmail.com">
